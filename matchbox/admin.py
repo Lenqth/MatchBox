205662d5b1952a7ccc171269383a1a154e3015a3
Lenqth <nullptr00@gmail.com>
2018-12-19 21:19:45 +0900

    import修正

--- matchbox/admin.py
+++ matchbox/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from games.jong.models import GameJongResult
+from .games.jong.models import GameJongResult
 admin.site.register(GameJongResult)
-
