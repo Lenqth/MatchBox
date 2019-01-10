3fe1a20b521b3718d437a752e90e64732d52dff1
Lenqth <nullptr00@gmail.com>
2019-01-10 12:13:39 +0900

    バリデーションとコードフォーマット

--- matchbox/games/jong/agent.py
+++ matchbox/games/jong/agent.py
@@ -7,7 +7,8 @@ from .judge.shanten import shanten
 
 from .structs import *
 
-import asyncio,promise
+import asyncio
+import promise
 from promise import Promise
 import time
 
@@ -15,32 +16,31 @@ import traceback
 import threading
 
 
-
-
 class AITsumogiri:
     def __init__(self):
         pass
 
-    def select_discard(self,pl,game):
+    def select_discard(self, pl, game):
         return len(pl.hand)-1
 
     def get_name(self):
         return "ツモ切りAIちゃん"
 
-    async def turn_command_async(self,pl,game,commands):
-        return TurnCommand(TurnCommand.DISCARD,-1)
+    async def turn_command_async(self, pl, game, commands):
+        return TurnCommand(TurnCommand.DISCARD, -1)
 
-    async def command_async(self,pl,game,commands):
+    async def command_async(self, pl, game, commands):
         return Claim(0)
 
-    async def confirm(self,message):
+    async def confirm(self, message):
         pass
 
-    async def message(self,message):
+    async def message(self, message):
         pass
 
-    async def send(self,obj):
+    async def send(self, obj):
         pass
+
 
 class AIShanten:
     def __init__(self):
@@ -49,78 +49,91 @@ class AIShanten:
     def get_name(self):
         return "よわいAIちゃん"
 
-    def select_discard(self,pl,game):
+    def select_discard(self, pl, game):
         ary = list_to_array(pl.hand)
-        e = np.eye( 64 , dtype = np.int32 )
-        q = np.tile(ary, (len(pl.hand),1) ) - e[pl.hand]
-        p = np.apply_along_axis( shanten , 1 , q )
+        e = np.eye(64, dtype=np.int32)
+        q = np.tile(ary, (len(pl.hand), 1)) - e[pl.hand]
+        p = np.apply_along_axis(shanten, 1, q)
         return np.argmin(p)
 
-    async def turn_command_async(self,pl,game,commands):
-        return TurnCommand(TurnCommand.DISCARD,-1)
+    async def turn_command_async(self, pl, game, commands):
+        return TurnCommand(TurnCommand.DISCARD, -1)
 
-    async def command_async(self,pl,game,commands):
+    async def command_async(self, pl, game, commands):
         return Claim(0)
 
-
-    async def confirm(self,message):
+    async def confirm(self, message):
         pass
 
-    async def message(self,message):
+    async def message(self, message):
         pass
 
-    async def send(self,obj):
+    async def send(self, obj):
         pass
+
 
 class RemotePlayer:
 
-    def __init__(self,connection):
+    def __init__(self, connection):
         self.conn = connection
 
     def get_name(self):
-        
-        return str( self.conn.get_user() )
 
-    def send(self,obj):
-        if not hasattr(self,"conn") :
+        return str(self.conn.get_user())
+
+    def send(self, obj):
+        if not hasattr(self, "conn"):
             raise Exception("No connection")
 
-    async def turn_command_async(self,pl,game,commands):
-        obj = { "type":"your_turn" , "hand_tiles":pl.hand , "draw":pl.drew , "turn_commands_available" : commands }
-        if pl.agari_infos is not None :
+    @staticmethod
+    def validate_command(command, li):
+        return command in li
+
+    async def turn_command_async(self, pl, game, commands):
+        obj = {"type": "your_turn", "hand_tiles": pl.hand,
+               "draw": pl.drew, "turn_commands_available": commands}
+        if pl.agari_infos is not None:
             obj["agari_info"] = pl.agari_infos
 
         try:
-            res = await self.conn.send_and_receive_reply( obj ,timeout=game.timeout)
+            res = await self.conn.send_and_receive_reply(obj, timeout=game.timeout)
             res = TurnCommand.fromDict(res)
+            if not RemotePlayer.validate_command(res, obj["commands_available"]):
+                raise Exception("validation error : %s" % res)
             return res
-        except Exception as e :
+        except Exception as e:
             traceback.format_exc()
-            return TurnCommand(TurnCommand.DISCARD,-1)
+            return TurnCommand(TurnCommand.DISCARD, -1)
 
-    async def command_async(self,pl,game,commands):
-        obj = { "type": "claim_command" , "hand_tiles":pl.hand , "target":{"player":game.turn, "apkong":game.apkong , "tile" : game.target_tile } , "commands_available" : commands }
-        if pl.agari_infos is not None :
+    async def command_async(self, pl, game, commands):
+        obj = {"type": "claim_command", "hand_tiles": pl.hand,
+               "target": {"player": game.turn,
+                          "apkong": game.apkong,
+                          "tile": game.target_tile},
+               "commands_available": commands}
+        if pl.agari_infos is not None:
             obj["agari_info"] = pl.agari_infos
         try:
-            res = await self.conn.send_and_receive_reply( obj ,timeout=game.timeout)
+            res = await self.conn.send_and_receive_reply(obj, timeout=game.timeout)
             res = Claim.fromDict(res)
+            if not RemotePlayer.validate_command(res, obj["commands_available"]):
+                raise Exception("validation error : %s" % res)
             return res
-        except Exception as e :
+        except Exception as e:
             traceback.format_exc()
             return Claim(0)
 
-    async def confirm(self,message):
-        obj = { "type": "confirm" , "message":message }
+    async def confirm(self, message):
+        obj = {"type": "confirm", "message": message}
         try:
-            await self.conn.send_and_receive_reply( obj ,timeout=3000)
-        except Exception as e :
+            await self.conn.send_and_receive_reply(obj, timeout=3000)
+        except Exception as e:
             traceback.format_exc()
             return
 
-    async def message(self,message):
-        obj = { "type": "message" , "message":message }
+    async def message(self, message):
+        obj = {"type": "message", "message": message}
         await self.conn.send(obj)
 
-    async def send(self,obj):
+    async def send(self, obj):
         await self.conn.send(obj)
